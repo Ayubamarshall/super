06d948d666a6c4eafc5b68f8d720ae3d40315e39
johninua <johninus90@gmail.com>
2021-06-06 16:27:38 -0700

    about, index

---
+++ about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>About</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1><strong>WELCOME TO DATABASE CONCEPT (CSC506) ASSIGNMENT</strong></h1>
+				<a href="home.php"><i class="fas fa-home"></i>Home</a>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-power-off"></i>Logout</a>
+			</div>
+		</nav>
+<marquee width="80%" direction="left" height="10px"scrollamount="3"><strong>Welcome To The Team; (JOHN INUSA, AYUBA MARSHALL, SANI A ALI, SADIQ A ABDULAZIZ, MOHAMMAD JIDDA)</strong>.
+
+</marquee>
+		<div class="content">
+			<h2>About</h2>
+
+<div>
+<img src="campus.jpg" alt="italian trulli">
+<div><a><i class="fas fa-user"></i> FULL NAME: <STRONG>JOHN INUSA, AYUBA MARSHALL, SANI A ALI, SADIQ A ABDULAZIZ, MOHAMMAD JIDDA </STRONG></a></div>
+<div><a><i class="fas fa-user-graduate"></i> DEPARTMENT: <STRONG>COMPUTER SCIENCE</STRONG></a></div>
+<div><a><i class="fas fa-home"></i> FACULTY: <STRONG>PHYSICAL SCIENCE</STRONG></a></div>
+<div><a><i class="fas fa-address-card"></i> I.D NUMBER: <STRONG>CSC/16D/4330,CSC/15U/3362, CSC/15U/3248, CSC/15/14/0435, CSC/15U/3379</STRONG></a></div>
+<div><a><i class="fas fa-phone"></i> PHONE: <STRONG>+23462941925  & +2348127734807</strong></a></div>
+
+<div><a><i class="fas fa-envelope"></i>EMAIL: <strong>Team@gmail.com</strong></a></div>
+
+<div><a><i class="fas fa-address-book"></i> HOME ADDRESS: <strong>NO.15 EPP PLAZA GEREI ROAD ADAMAWA STATE</strong></a></div>
+
+JOHN INUSA, AYUBA MARSHALL, SANI A ALI, SADIQ A ABDULAZIZ AND MOHAMMAD JIDDA we are student of Modibbo Adama University Of Technology Yola, School Of Physical Science Department of Computer Science. We have strong passion for 
+Web development and programming.
+<div>Thank You...!</div>
+</div>
+		</div>
+	</body>
+</html>
